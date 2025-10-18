f9bf93b8e624e9a174a3781758471a7478289f6a
Carlos osmar Caceres <omarcaceres@live.com>
2025-10-18 13:56:26 -0300

    add services in entities

---
+++ domain/src/entities/services.ts
@@ -0,0 +1,9 @@
+export interfaces Services {
+    id: string;
+    title: string;
+    subtitle: string;
+    description: string;
+    price: number;
+    categoryId: string;
+    plans: string[];
+}
